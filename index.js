cab5b01e27a9feeafe257497b5c928c8df1c8de4
Finn Manapat <113877935+chastlyregino@users.noreply.github.com>
2022-10-11 09:34:28 +0800

    Using process.argv, count the arguments and vowels that is passed in CLI (#1)
    Using process.argv, count the arguments and vowels that is passed in CLI

---
+++ index.js
@@ -0,0 +1,19 @@
+const { argv } = require('node:process');
+
+argv.shift();
+argv.shift();
+
+console.log(`Number of Arguments: ${argv.length}`);
+
+const str = argv.join(' ');
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let count = 0;
+
+for (let char of str.toLowerCase()) {
+  if (vowels.includes(char)) {
+    count++;
+  }
+}
+
+console.log(`Number of Vowels: ${count}`);
